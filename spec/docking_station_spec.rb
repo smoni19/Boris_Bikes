4cd86493c81dcb2d98c37780b3ce983441438fcf
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-20 18:47:00 +0200

    Challenge 12 - Raising exceptions completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,34 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
+  
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-  it 'is bike working' do
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
-    expect(bike).to respond_to(:working?)
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
-  #it { is_expected.to respond_to(:dock).with(1).argument }
+  #it 'is bike working' do
+    #docking_station = DockingStation.new
+    #bike = docking_station.release_bike
+    #expect(bike).to respond_to(:working?)
+  #end
 
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  #it { is_expected.to respond_to(:bike) }
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
 end 
+
