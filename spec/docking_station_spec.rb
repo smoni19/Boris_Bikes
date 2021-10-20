2838bca5b5b1251f0ed1a85f1fd84f81967c4c4d
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-20 15:39:00 +0200

    Challenge 7 - add first it test and release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,6 @@
 require 'docking_station'
 
-=begin
-Create a new file for a Docking Station class, inside the /lib directory
- Define a DockingStation class
- Use require to include this file inside your spec file
- Run RSpec from the Command Line
- Explain to your pair partner the difference between what you see, and the error you saw before.
-=end
-
 describe DockingStation do
-  #it 'can store bikes' do
-    #docking_station = DockingStation.new
-  #end
+  it { is_expected.to respond_to(:release_bike) }
+
 end 
