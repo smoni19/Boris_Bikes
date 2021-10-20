f7d55d07d243e155bd681322da85a7feecbbc900
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-20 16:09:30 +0200

    Challenge 10 - make docking stations release a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,10 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
+  it 'is bike working' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).to respond_to(:working?)
+  end
+
 end 
