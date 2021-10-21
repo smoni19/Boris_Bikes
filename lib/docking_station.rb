4865ba8cab0782623fe78e74d21119117682e226
Simon Jacobs <simonjacobs@simons-air.home>
2021-10-21 15:47:29 +0100

    Challenge 15 - the SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -10,13 +10,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes_arr.empty?
+    fail 'No bikes available' if self.empty?
     @bikes_arr.pop
   end
 
   def dock(docked_bike)
-    fail 'No capacity at docking station' if @bikes_arr.count >= @capacity
+    fail 'No capacity at docking station' if self.full?
     @bikes_arr << docked_bike
     docked_bike
+  end
+
+  private
+  
+  def full?
+    @bikes_arr.count >= @capacity
+  end
+
+  def empty?
+    @bikes_arr.empty?
   end
 end
